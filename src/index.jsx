25b0d8d2f11a900ec61df518cf1b8744d72c465f
danikyakimenko <danialyakimenko@gmail.com>
2025-05-24 10:15:26 +0200

    deleted propTypes from index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -28,18 +28,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-
-// App.propTypes = {
-//   user: PropTypes.objectOf({
-//     username: PropTypes.string.isRequired,
-//     tag: PropTypes.string.isRequired,
-//     location: PropTypes.string.isRequired,
-//     avatar: PropTypes.string.isRequired,
-//     stats: PropTypes.objectOf({
-//       followers: PropTypes.number.isRequired,
-//       views: PropTypes.number.isRequired,
-//       likes: PropTypes.number.isRequired,
-//     }).isRequired,
-//   }),
-// };
